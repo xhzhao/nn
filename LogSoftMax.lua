d72f9173f17f53e949c896a1a512b88903dc4a80
wentingj <wenting.jiang@intel.com>
2016-09-22 09:43:17 +0800

    add LogSoftMax profile marker

--- LogSoftMax.lua
+++ LogSoftMax.lua
@@ -1,19 +1,38 @@
 local LogSoftMax = torch.class('nn.LogSoftMax', 'nn.Module')
 
+function LogSoftMax:__init()
+   self.gradInput = torch.Tensor()
+   self.output = torch.Tensor()
+   self._type = self.output:type()
+   self:setEngine(1)
+end
+
 function LogSoftMax:updateOutput(input)
+   local startTime = sys.clock()
    input.THNN.LogSoftMax_updateOutput(
       input:cdata(),
       self.output:cdata()
    )
+   if self.timerEnable then
+   		print("LogSoftMax forward time =         ,",self.timeForward," backward time =",self.timeBackward)
+                sys.logsoftmaxTime_forward = sys.logsoftmaxTime_forward + self.timeForward
+                sys.logsoftmaxTime_backward = sys.logsoftmaxTime_backward + self.timeBackward
+                self.timeForward = (sys.clock() - startTime)
+                self.cnt = self.cnt + 1
+   end
    return self.output
 end
 
 function LogSoftMax:updateGradInput(input, gradOutput)
+   local startTime = sys.clock()
    input.THNN.LogSoftMax_updateGradInput(
       input:cdata(),
       gradOutput:cdata(),
       self.gradInput:cdata(),
       self.output:cdata()
    )
+   if self.timerEnable then
+        self.timeBackward = (sys.clock() - startTime)
+   end
    return self.gradInput
 end
