2aab0a52978717fa712d85f1197c5e133a9a2e94
Xiaohui Zhao <xiaohui.zhao@intel.com>
2017-06-25 13:20:30 +0800

    merge menglin nn code

--- lib/THNN/generic/Sigmoid.c
+++ lib/THNN/generic/Sigmoid.c
@@ -19,10 +19,25 @@ void THNN_(Sigmoid_updateGradInput)(
 {
   THNN_CHECK_NELEMENT(output, gradOutput);
   THTensor_(resizeAs)(gradInput, output);
-  TH_TENSOR_APPLY3(real, gradInput, real, gradOutput, real, output,
-    real z = *output_data;
-    *gradInput_data = *gradOutput_data * (1. - z) * z;
-  );
+  ptrdiff_t gradInputSize = THTensor_(nElement)(gradInput);                     
+  ptrdiff_t gradOutputSize = THTensor_(nElement)(gradOutput);   
+  ptrdiff_t outputSize = THTensor_(nElement)(output);                    
+  int gradInputContig = THTensor_(isContiguous)(gradInput)? 1:0;                 
+  int gradOutputContig = THTensor_(isContiguous)(gradOutput)? 1:0;   
+  int outputContig = THTensor_(isContiguous)(output)? 1:0; 
+  if((gradInputSize = gradOutputSize) && (gradInputSize == outputSize) ){	
+    TH_TENSOR_APPLY3_ADVANCED_INDEX2(outputSize, gradInputContig, gradOutputContig, outputContig,
+                                     real, gradInput, real, gradOutput, real, output,
+                                     real z = *output_data;
+                                     *gradInput_data = *gradOutput_data * (1. - z) * z;
+    );
+  } else {
+    TH_TENSOR_APPLY3(real, gradInput, real, gradOutput, real, output,
+      real z = *output_data;
+      *gradInput_data = *gradOutput_data * (1. - z) * z;
+    );
+	  
+  }
 }
 
 #endif
